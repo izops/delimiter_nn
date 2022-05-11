991460e9f990b7ee00aa23a07c07e0e805e34796
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-05-11 14:15:42 +0200

    add file test path

--- codes/process_data.py
+++ codes/process_data.py
@@ -3,8 +3,8 @@
 import os, csv
 
 # define path of the source file
-strPathIn = ''
-strPathOut = ''
+strPathIn = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/sources/in1.txt'
+strPathOut = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/output/out1.txt'
 
 # check if the input file exists
 assert os.path.isfile(strPathIn)
@@ -12,4 +12,4 @@ assert os.path.isfile(strPathIn)
 # make sure the output file doesn't exist (to not overwrite)
 assert os.path.isfile(strPathOut) == False
 
-# 
+#

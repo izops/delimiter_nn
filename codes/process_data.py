de5433d8674dbf57762eb49f9c6feab76d52e95b
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-05-11 14:27:37 +0200

    add basic file operations with source and target files

--- codes/process_data.py
+++ codes/process_data.py
@@ -2,14 +2,29 @@
 
 import os, csv
 
+# define modes to use ('append' vs 'overwrite')
+strMode = 'append'
+
 # define path of the source file
-strPathIn = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/sources/in1.txt'
+strPathIn = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/sources/MP_AU_VERTRAG_INVENTUR_20210312.asc'
 strPathOut = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/output/out1.txt'
 
 # check if the input file exists
-assert os.path.isfile(strPathIn)
+assert os.path.isfile(strPathIn), 'Source doesn\'t exist'
 
-# make sure the output file doesn't exist (to not overwrite)
-assert os.path.isfile(strPathOut) == False
+# open the source file, read only
+objIn = open(strPathIn, 'r')
 
-#
+# open the target file in append mode, if selected
+if strMode == 'append' and os.path.isfile(strPathOut):
+    objOut = open(strPathOut, 'a')
+else:
+    objOut = open(strPathOut, 'w')
+
+
+# save and close the target file
+objOut.save()
+objOut.close()
+
+# close the source file
+objIn.close()

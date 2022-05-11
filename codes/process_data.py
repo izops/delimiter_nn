41b61b6e245d65ef7ac1cdba061ee220d7def734
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-05-11 14:06:49 +0200

    create sceleton of the data prep function

---
+++ codes/process_data.py
@@ -0,0 +1,13 @@
+# create large one-column file for data generating
+
+import os, csv
+
+# define path of the source file
+strPathIn = ''
+strPathOut = ''
+
+# check if the input file exists
+assert os.path.isfile(strPathIn)
+
+# make sure the output file doesn't exist (to not overwrite)
+assert os.path.isfile(strPathOut) == False

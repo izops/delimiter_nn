310bf8e7bd7b4ea77b876bfec53d5e2d16c071b8
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-05-20 15:30:02 +0200

    add loop for data creation based on the number of required output rows

--- codes/generate_data.py
+++ codes/generate_data.py
@@ -13,12 +13,13 @@ def GenerateData(
     pintNumFiles,
     pintNumFileRows,
     pstrPathOut,
-    pintNumCols
+    pintNumOutRows,
+    pintNumOutCols
 ):
     assert os.path.isdir(pstrPathSource), 'Provide a path with source files'
     assert pintNumFiles > 0, 'No source files, can\'t continue'
     assert os.path.isdir(pstrPathOut), 'Provide an output path'
-    assert pintNumCols > 0, 'There has to be at least one column in the output'
+    assert pintNumOutCols > 0, 'There has to be at least one column in the output'
 
     # create output file path
     if pstrPathOut[-1] != '/':
@@ -30,50 +31,67 @@ def GenerateData(
     objData = open(strPath + STR_FILE_NAME_DATA, 'w')
     objLabels = open(strPath + STR_FILE_NAME_LABELS, 'w')
 
-    # generate a random number based on the number of source files
-    intFile = int(np.random.uniform() * pintNumFiles)
+    for intTotalRows in range(pintNumOutRows):
+        # generate a random number based on the number of source files
+        intFile = int(np.random.uniform() * pintNumFiles)
 
-    # generate full name of the generated source file
-    strFileIn = strPath + STR_FILE_NAME_IN + str(intFile) + STR_FILE_EXTENSION_IN
+        # generate full name of the generated source file
+        strFileIn = strPath + STR_FILE_NAME_IN + str(intFile) +
+            STR_FILE_EXTENSION_IN
 
-    # initialize empty lists for the file and for the output
-    lstFile = list()
-    lstOut = list()
+        # initialize empty lists for the file and for the output
+        lstFile = list()
+        lstOut = list()
 
-    # open the source file
-    objInFile = open(strFileIn)
+        # open the source file
+        objInFile = open(strFileIn)
 
-    # read the file into the list
-    for strRow in objInFile:
-        lstFile.append(strRow)
+        # read the file into the list
+        for strRow in objInFile:
+            lstFile.append(strRow)
 
-    # generate a random array with possible row indexes
-    npRowIndexes = np.random.rand(pintNumWords) * len(lstFile)
-    npRowIndexes = npRowIndexes.astype(int)
+        # close the source file
+        lstFile.close()
 
-    # generate a random delimiter and its label to
+        # generate a random array with possible row indexes
+        npRowIndexes = np.random.rand(pintNumWords) * len(lstFile)
+        npRowIndexes = npRowIndexes.astype(int)
 
-    # get the respective words and write them to data and labels
-    for intIndex in range(len(npRowIndexes)):
-        # get the selected word
-        strWord = lstFile[npRowIndexes[intIndex]]
+        # generate a random delimiter and its label for this data row
+        strDelim, intLabel = fRandomDelim()
 
-        # write selected word to data and generate respective labels
-        WriteToFiles(objData, objLabels, strWord)
+        # get the respective words and write them to data and labels
+        for intIndex in range(len(npRowIndexes)):
+            # get the selected word
+            strWord = lstFile[npRowIndexes[intIndex]]
 
-        # add the last symbol based on the position of the loop
-        if intIndex == len(npRowIndexes) - 1:
-            # last word, add line break
-            objData.write('\n')
-            objLabels.write('\n')
-        else:
-            # not the last word, add delimiter and its label
+            # write selected word to data and generate respective labels
+            WriteToFiles(objData, objLabels, strWord)
+
+            # add the last symbol based on the position of the loop
+            if intIndex == len(npRowIndexes) - 1:
+                # last word, add line break
+                objData.write('\n')
+                objLabels.write('\n')
+            else:
+                # not the last word, add delimiter and its label
+                objData.write(strDelim)
+                objLabels.write(str(intLabel) + ' ')
 
 
 
     # get a data row from the source file
 
 def WriteToFiles(pobjData, pobjLabels, pstrWord):
+    '''
+    Writes a word to the data file and corresponding zero labels to the labels
+    file
+
+    Inputs:
+        pobjData - handle to a data file opened for writing
+        pobjLabels - handle to a label file opened for writing
+        pstrWord - word to be written and labeled
+    '''
     # do the operation only if the word is not missing
     if len(pstrWord) > 0:
         # generate array of zeros with as many zeros as characters in the word

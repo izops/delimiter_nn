cb6ca99eea3caef73779e57e71ca6570a6b4486f
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-05-27 16:27:15 +0200

    add function to zero-pad a string

--- codes/data_preparation.py
+++ codes/data_preparation.py
@@ -1,7 +1,7 @@
 # process the data for neural network
 import os
 
-def flstReadData(pstrPath):
+def flstReadData(pstrPath, zero_pad = True, required_length = 100):
     '''
     Imports a text file to a list
 
@@ -44,3 +44,34 @@ def flstConvertToASCII(plstData):
         lstOut.append(list(map(ord, lstSublist)))
 
     return lstOut
+
+def flstListifyRow(pstrRow, zero_pad = True, required_length = 100):
+    assert type(pstrRow) == str, 'Not a string, can\'t continue'
+
+    # remove the line breaks from the string
+    strProcessed = pstrRow.replace('\n', '')
+
+    # modify the string to match length requirements
+    if zero_pad:
+        if len(strProcessed) < required_length:
+            # zero padding required, match it
+            strProcessed = fstrZeroPad(strProcessed, required_length)
+
+def fstrZeroPad(pstrInput, pintLength):
+    '''
+    Create zero padding of a string to required length.
+
+    Inputs:
+        pstrInput - input string to zero pad
+        pintLength - length to which the string will be zero padded
+
+    Output:
+        String zero-padded to a required
+    '''
+    # create zero padding with the required length
+    strZeros = '0' * (pintLength - len(pstrInput))
+
+    # concatenate the strings
+    strProcessed = pstrInput + strZeros
+
+    return strProcessed

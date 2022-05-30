7405043868c595d318fe1d853de34b9f9f2e889b
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-05-30 14:42:24 +0200

    test processing of the input data

--- codes/nn_model.py
+++ codes/nn_model.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 import data_preparation as dp
 
 # set paths
-strPathData = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/output/sample_data.txt'
-strPathLabels = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/output/sample_labels.txt'
+strPathData = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/output/data.txt'
+strPathLabels = 'c:/Users/ivan.zustiak/OneDrive - Zurich Insurance/snake/emea_oth_nn_separator/data/output/labels.txt'
 
 # import data
 lstData = dp.flstReadData(strPathData)
@@ -14,5 +14,5 @@ lstLabels = dp.flstReadData(strPathLabels, True)
 train_data = tf.data.Dataset.from_tensor_slices(lstData)
 train_labels = tf.data.Dataset.from_tensor_slices(lstLabels)
 
-for e in train_data:
+for e in train_labels:
     print(e)

c9a5c21aeabeea3b6847f70fd9345c6c49df706d
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-06-07 15:57:39 +0200

    add tf neural net example

--- codes/test.py
+++ codes/test.py
@@ -1,34 +1,47 @@
 # test the keras tutorial
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
 (x_train, y_train), (x_val, y_val) = keras.datasets.fashion_mnist.load_data()
 
-print(type(x_train[0]))
+print(np.shape(x_train))
 
-# def preprocess(x, y):
-#   x = tf.cast(x, tf.float32) / 255.0
-#   y = tf.cast(y, tf.int64)
-#
-#   return x, y
-#
-# def create_dataset(xs, ys, n_classes=10):
-#   ys = tf.one_hot(ys, depth=n_classes)
-#   return tf.data.Dataset.from_tensor_slices((xs, ys)) \
-#     .map(preprocess) \
-#     .shuffle(len(ys)) \
-#     .batch(128)
-#
-#
-# train_dataset = create_dataset(x_train, y_train)
-# val_dataset = create_dataset(x_val, y_val)
-#
-# intCount = 0
-#
-# for i in x_train:
-#     print(type(i))
-#
-#     if intCount == 0:
-#         break
-#     else:
-#         intCount += 1
+def preprocess(x, y):
+  x = tf.cast(x, tf.float32) / 255.0
+  y = tf.cast(y, tf.int64)
+
+  return x, y
+
+def create_dataset(xs, ys, n_classes=10):
+  ys = tf.one_hot(ys, depth=n_classes)
+  return tf.data.Dataset.from_tensor_slices((xs, ys)) \
+    .map(preprocess) \
+    .shuffle(len(ys)) \
+    .batch(128)
+
+
+train_dataset = create_dataset(x_train, y_train)
+val_dataset = create_dataset(x_val, y_val)
+
+print(tf.shape(train_dataset))
+
+model = keras.Sequential([
+    keras.layers.Reshape(target_shape=(28 * 28,), input_shape=(28, 28)),
+    keras.layers.Dense(units=256, activation='relu'),
+    keras.layers.Dense(units=192, activation='relu'),
+    keras.layers.Dense(units=128, activation='relu'),
+    keras.layers.Dense(units=10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(
+    train_dataset.repeat(),
+    epochs=10,
+    steps_per_epoch=500,
+    validation_data=val_dataset.repeat(),
+    validation_steps=2
+)

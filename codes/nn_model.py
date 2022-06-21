7559376d4c11eef0d3f9d468eab440639cd0ccb1
Ivan Zustiak <ivan.zustiak@zurich.com>
2022-06-21 16:26:59 +0200

    fine tune parameters of the nn to achieve 90% categorical accuracy on the training data

--- codes/nn_model.py
+++ codes/nn_model.py
@@ -58,14 +58,11 @@ print('Conversion to tensors finished')
 model = keras.Sequential([
     keras.layers.Reshape(target_shape = (100,), input_shape = (100, )),
     keras.layers.Dense(units = 100, activation = 'relu'),
-    keras.layers.Dense(units = 100, activation = 'relu'),
+    keras.layers.Dense(units = 128, activation = 'relu'),
+    keras.layers.Dense(units = 128, activation = 'relu'),
     keras.layers.Dense(units = 64, activation = 'relu'),
     keras.layers.Dense(units = 64, activation = 'relu'),
-    keras.layers.Dense(units = 64, activation = 'relu'),
-    keras.layers.Dense(units = 64, activation = 'relu'),
-    keras.layers.Dense(units = 48, activation = 'relu'),
-    keras.layers.Dense(units = 48, activation = 'relu'),
-    keras.layers.Dense(units = 48, activation = 'relu'),
+    keras.layers.Dense(units = 128, activation = 'relu'),
     keras.layers.Dense(units = 48, activation = 'relu'),
     keras.layers.Dense(units = 16, activation = 'relu'),
     keras.layers.Dense(units = INT_NUM_CLASSES, activation = 'softmax')
@@ -83,5 +80,6 @@ trained = model.fit(
     train_x,
     train_y,
     epochs = 20,
-    batch_size = 128
+    batch_size = 128,
+    validation_data = test_data
 )
